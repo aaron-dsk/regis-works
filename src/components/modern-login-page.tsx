c2e100d29be7869f4a8b4e871fca788909eca97b
aaron-dsk <aaron@dsklabs.com>
2024-10-16 20:54:02 -0300

    Changed: navigation after login

--- src/components/modern-login-page.tsx
+++ src/components/modern-login-page.tsx
@@ -27,7 +27,7 @@ export function ModernLoginPageComponent() {
 
     if (isValidCredential) {
       localStorage.setItem('isLoggedIn', 'true')
-      router.push('/offerings')  // Update this line
+      router.push('/recommended-jobs')  // Update this line
     } else {
       setError('Username or password invalid')
     }
